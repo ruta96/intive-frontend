d52ca5667f9f51488c2e3177592fdc12822d22dc
Michał Ruta <ruciak1996@gmail.com>
2021-01-29 02:05:36 +0100

    added listing elements from data

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let seenCoutner = 0, allCounter = moviesData.length;
 
 //count seen movies
-for (let i = 0; i<moviesData.length; i++){
-    if (moviesData[i].seen === "T"){
+moviesData.forEach( elem => {
+    if (elem.seen === "T"){
     seenCoutner++;
     }
-}
+});
 
 //send value of counters to html
 document.getElementById("moviesCounterAll").innerHTML = allCounter;
 document.getElementById("moviesCounterSeen").innerHTML = seenCoutner;
 
+//load data to html
+    moviesData.forEach(elem => {
+        document.getElementById('moviesList').innerHTML += `
+        <li class="moviesListElement">${elem.title}</br> ${elem.year}</br> ${elem.genre}</br> ${elem.summary}</br></br></br></li>
+        `
+    })
