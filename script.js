0e34bc440a8168909bb71988b7d6c5cdbd119a67
Michał Ruta <ruciak1996@gmail.com>
2021-01-31 06:41:03 +0100

    onclick and function that handle it | change seen movies

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-let seenCoutner = 0, allCounter = moviesData.length;
+allCounter = moviesData.length;
 let watchStatus = '';
 
+
 //count seen movies
+const countSeen = () => {
+    let seenCoutner = 0
     moviesData.forEach( elem => {
-        if (elem.seen === "T"){
+        if (elem.seen == "T"){
         seenCoutner++;
         }
     });
 
+
+    
+document.getElementById("moviesCounterSeen").innerHTML = seenCoutner;
+};
+
+//first initialize of function counting seen movies
+countSeen();
+
 //send value of counters to html
 document.getElementById("moviesCounterAll").innerHTML = allCounter;
-document.getElementById("moviesCounterSeen").innerHTML = seenCoutner;
+
 
 //load data to html
-    moviesData.forEach((elem) => {
+    moviesData.forEach((elem, index) => {
         
+//initial class assigment for seen and unseen movies
         if (elem.seen === "T")
             watchStatus = "seen"
         else
             watchStatus ="unseen"
+
+//template of list element
         document.getElementById('moviesList').innerHTML += (
             `<li class="listElement">
                     <h1>${elem.title}</h1>
-                    <h2 class="status">status: ${watchStatus} <img src="img/eye_${watchStatus}.svg" alt="${watchStatus}"></h2>
+                    <h2 class="status">status: <span class="${watchStatus}" id="${index}" onclick="changeStatus(this.id, this.className)"></span></h2>
                     <h2>year: ${elem.year}</h2>
                     <h2>genre: ${elem.genre}</h2>
                     <h2>description:</h2><p>${elem.summary}</p>
@@ -30,11 +44,16 @@ document.getElementById("moviesCounterSeen").innerHTML = seenCoutner;
             )
     });
 
-//change watch status
-const changeMovieStatus = () => {
-console.log("clicked");
-};
-
-const statusImg = document.querySelector(".status > img");
-
-statusImg.onclick = changeMovieStatus;
+ //onclick handle  
+ //change img of seen status on page | change value on key seen in table moviesData 
+ const changeStatus = (id, elementClass) => {
+    if (elementClass === "seen"){
+        moviesData[id].seen = "F";
+        document.getElementById(id).className ="unseen";
+    }
+    else {
+        moviesData[id].seen = "T";
+        document.getElementById(id).className ="seen";
+    }
+    countSeen();
+}   
