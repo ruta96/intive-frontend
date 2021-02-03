f8758e62928a29fff20a95d35e1de86dbfe67446
Michał Ruta <ruciak1996@gmail.com>
2021-02-03 01:49:41 +0100

    syntax and prefixes
    some syntax improvement
    added css prefixes
    added favicon
    added few meta tags to html

--- script.js
+++ script.js
@@ -8,37 +8,37 @@ document.getElementById("moviesCounterSeen").innerHTML = moviesData.filter(elem 
 countMovies();
 
 moviesData.forEach((elem, index) => {
-//initial class assigment for seen and not seen movies
-    if (elem.seen === "T")
+    //initial class assigment for seen and not seen movies
+    if (elem.seen === "T"){ 
         watchStatus = "seen"
-    else
+    }
+    else{ 
         watchStatus ="not-seen"
-
-//template of single list element
+    }
+    //template of single list element
     document.getElementById('moviesList').innerHTML += (`
         <li class="listElement">
-            <div class="${watchStatus}" id="${index}"></div>
             <h1>${elem.title} (${elem.year})</h1>
+            <div class="${watchStatus}" id="${index}"></div>
             <h2 class="genre">${elem.genre}</h2>
             <hr>
             <p>${elem.summary}</p>
         </li>
         `)
-    });
+});
 
 //handle onclick
 //change status img and value in table at key seen
-    for(let i = 0; i<moviesData.length; i++){
-        document.getElementById(i).addEventListener('click',
-        function () {
+for(let i = 0; i<moviesData.length; i++){
+    document.getElementById(i).addEventListener('click',function () {
         if (this.className === "seen"){
-                moviesData[this.id].seen = "F";
-                this.className ="not-seen";
-           }
+            moviesData[this.id].seen = "F";
+            this.className ="not-seen";
+        }
         else {
-                moviesData[this.id].seen = "T";
-                this.className = "seen";
-           }
+            moviesData[this.id].seen = "T";
+            this.className = "seen";
+        }
         countMovies();
-        })
-    }
+    })
+}
